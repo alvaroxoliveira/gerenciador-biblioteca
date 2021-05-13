1989cd3fdce27e20aaf5f82118d7574d3cd24f52
Caio Nery <caionms@ufba.br>
2021-05-13 16:33:43 -0300

    Estados de livros atualizados para as impressões.

--- src/Estado/Emprestado.java
+++ src/Estado/Emprestado.java
@@ -3,16 +3,16 @@ package Estado;
 public class Emprestado implements EstadoLivro{
     @Override
     public void emprestarLivro() {
-
+        System.out.println("Não é possível fazer empréstimo de um livro emprestado.");
     }
 
     @Override
     public void devolverLivro() {
-
+        System.out.println("Devolução concluída com sucesso");
     }
 
     @Override
     public void reservarLivro() {
-
+        System.out.println("Não é possível fazer reserva de um livro emprestado.");
     }
 }
--- src/Estado/Reservado.java
+++ src/Estado/Reservado.java
@@ -3,16 +3,16 @@ package Estado;
 public class Reservado implements EstadoLivro{
     @Override
     public void emprestarLivro() {
-
+        System.out.println("Empréstimo concluído com sucesso.");
     }
 
     @Override
     public void devolverLivro() {
-
+        System.out.println("Não é possível devolver um livro disponível.");
     }
 
     @Override
     public void reservarLivro() {
-
+        System.out.println("Reserva concluída com sucesso.");
     }
 }
