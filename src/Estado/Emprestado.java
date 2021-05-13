13f4ce9298ccdd1a69eca3296ba626d3cde84d44
Caio Nery <caionms@ufba.br>
2021-05-13 16:22:00 -0300

    Estados do livro adicionados.

---
+++ src/Estado/Emprestado.java
@@ -0,0 +1,18 @@
+package Estado;
+
+public class Emprestado implements EstadoLivro{
+    @Override
+    public void emprestarLivro() {
+
+    }
+
+    @Override
+    public void devolverLivro() {
+
+    }
+
+    @Override
+    public void reservarLivro() {
+
+    }
+}
---
+++ src/Estado/Livre.java
@@ -0,0 +1,18 @@
+package Estado;
+
+public class Livre implements EstadoLivro{
+    @Override
+    public void emprestarLivro() {
+
+    }
+
+    @Override
+    public void devolverLivro() {
+
+    }
+
+    @Override
+    public void reservarLivro() {
+
+    }
+}
