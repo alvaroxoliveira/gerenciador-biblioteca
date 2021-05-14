cae07a2aa42972cc95ee11d60566085fb997035c
Alvaro Oliveira <alvaro.oliveira@ufba.br>
2021-05-14 02:43:24 -0300

    Inclusão da pasta .out no gitignore e pequeno teste feito na main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import Usuario.*;
 public class Main {
 
     public static void main(String[] args) {
-
+        IAlunos p1 = new AlunoGraduacao(100, "Alvaro Souza Oliveira");
+        System.out.println(p1.getQuantidadeDeLivrosEmprestadosDoUsuario());
     }
 }
