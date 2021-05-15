c042d4010306fe4a7805b3be74090e9aae5f4d25
Alvaro Oliveira <alvaro.oliveira@ufba.br>
2021-05-15 16:56:13 -0300

    Refatoração no código para correção de erro na implementação dos métodos das subclasses de usuário

--- src/Usuario/IUsuario.java
+++ src/Usuario/IUsuario.java
@@ -1,6 +1,18 @@
 package Usuario;
 
+import Livro.Livro;
+
+import java.util.ArrayList;
+
 public interface IUsuario {
+    public void pegaEmprestado(Livro livro);
+    public void devolveLivro(int i);
+    public int getIdentificador();
+    public void setIdentificador(int identificador);
+    public String getNome();
+    public void setNome(String nome);
+    public ArrayList<Livro> getListaDeLivrosEmprestados();
+    public void setListaDeLivrosEmprestados(ArrayList<Livro> listaDeLivrosEmprestimo);
     public void emprestimo();
     public void devolucao();
     public void reserva();
