a57fd8521ac8349c451454848f452da06cb2fb5d
Alvaro Oliveira <alvaro.oliveira@ufba.br>
2021-05-24 18:19:01 -0300

    Alteração na interface subject para a recepção do parametro de nome de usuário pelo metodo que adiciona usuário

--- src/Observer/Subject.java
+++ src/Observer/Subject.java
@@ -1,6 +1,6 @@
 package Observer;
 
 public interface Subject {
-        void adicionarObserver(Observer observer);
+        void adicionarObserver(Observer observer, String nomeUser);
         void notificarObserver();
 }
