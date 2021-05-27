db03c4934f9772ad9ce247a3e9a2060a3e0a0a62
Alvaro Oliveira <alvaro.oliveira@ufba.br>
2021-05-27 15:51:50 -0300

    Mudanças nas buscas e refatoração nos codigos e remoção da busca de testes

--- src/Buscas/BuscaLivro.java
+++ src/Buscas/BuscaLivro.java
@@ -2,11 +2,10 @@ package Buscas;
 
 import Livro.CarregaLivros;
 import Livro.Livro;
-import MensagensConsole.MensagensBuscaLivro;
 
 public class BuscaLivro {
     //Busca pelo id do livro na lista de livroe e compara com o id passado
-    private static int buscaIndexDoLivro(String identificacaoLivro) {
+    public static int buscaIndexDoLivro(String identificacaoLivro) {
         int index = 0;
         for(Livro livro: CarregaLivros.getLivrosDoSistema()) {
             if(identificacaoLivro.equals(livro.getId())) {
@@ -17,28 +16,8 @@ public class BuscaLivro {
         return -1;
     }
 
-    //Retorna o objeto livro (caso exista) compativel com o id passado
     public static Livro getLivro(String identificacaoLivro) {
         int indexDoLivro = buscaIndexDoLivro(identificacaoLivro);
         return CarregaLivros.getLivrosDoSistema().get(indexDoLivro);
-//        if(indexDoLivro != -1) {
-//            return CarregaLivros.getLivrosDoSistema().get(indexDoLivro);
-//        }
-//        /*else {
-//            System.out.println("Livro não existe.");
-//        }*/
-//        // verificar se tem que tratar esse retorno de null
-//        return null;
-    }
-
-    //Testa se o id do livro passado esta na lista de livros
-    public static boolean testeLivro(String identificadorLivro){
-        for(Livro livro: CarregaLivros.getLivrosDoSistema()) {
-            if (livro.getId().equals(identificadorLivro)) {
-                return true;
-            }
-        }
-        MensagensBuscaLivro.mensagemLivroNaoEncontrado();
-        return false;
     }
 }
--- src/Buscas/BuscaUsuario.java
+++ src/Buscas/BuscaUsuario.java
@@ -1,12 +1,11 @@
 package Buscas;
 
-import MensagensConsole.MensagensBuscaUsuario;
 import Usuario.CarregaUsuarios;
 import Usuario.User;
 
 public class BuscaUsuario {
     //Busca pelo id do usuário na lista de usuários e compara com o id passado
-    private static int buscaIndexDoUsuario(String identificacaoUsuario) {
+    public static int buscaIndexDoUsuario(String identificacaoUsuario) {
         int index = 0;
         for(User user: CarregaUsuarios.getUsuariosDoSistema()) {
             if(identificacaoUsuario.equals(user.getIdentificador())) {
@@ -17,28 +16,8 @@ public class BuscaUsuario {
         return -1;
     }
 
-    //Retorna o objeto usuário (caso exista) compativel com o id passado
     public static User getUsuario(String identificacaoUsuario) {
         int indexDoUsuario = buscaIndexDoUsuario(identificacaoUsuario);
         return CarregaUsuarios.getUsuariosDoSistema().get(indexDoUsuario);
-//        if(indexDoUsuario != -1) {
-//            return CarregaUsuarios.getUsuariosDoSistema().get(indexDoUsuario);
-//        }
-//        /*else {
-//            System.out.println("Usuario não existe.");
-//        }*/
-//        // verificar se tem que tratar esse retorno de null
-//        return null;
-    }
-
-    //Testa se o id do usuário passado está na lista de usuários
-    public static boolean testeUsuario(String identificadorUsuario){
-        for(User usuario: CarregaUsuarios.getUsuariosDoSistema()){
-            if(usuario.getIdentificador().equals(identificadorUsuario)){
-                return true;
-            }
-        }
-        MensagensBuscaUsuario.mensagemUsuarioNaoEncontrado();
-        return false;
     }
 }
