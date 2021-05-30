36154af0d44d0645bb3713a0eb53f7aff24dceca
Alvaro Oliveira <alvaro.oliveira@ufba.br>
2021-05-30 06:41:07 -0300

    Adicionado comentário na Interface de EstadoLivro, que faltava

--- src/Livro/Estado/IEstadoLivro.java
+++ src/Livro/Estado/IEstadoLivro.java
@@ -1,5 +1,10 @@
 package Livro.Estado;
 
+/*
+* Interface que define as funções específicas implementadas pelos estados.
+* Base do Padrão de Projeto State, ensinado em aula.
+* */
+
 import Livro.Exemplar;
 import Usuario.Usuario;
 
